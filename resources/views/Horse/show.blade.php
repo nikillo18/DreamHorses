174f5aa3e2cadb72f1918188bbb1102c706ef05b
Nicolas <nicodrax18@gmail.com>
2025-09-24 19:23:11 -0300

    se agrego una imagen del contorno de un caballo para saber que no hay una foto subida

--- resources/views/Horse/show.blade.php
+++ resources/views/Horse/show.blade.php
@@ -41,7 +41,7 @@
                                 @endforeach
                             </div>
                         @else
-                            <img src="{{ $horse->photo_path ? asset('storage/' . $horse->photo_path) : 'https://images.unsplash.com/photo-1615989275591-9fdbfe661ec1?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}"
+                            <img src="{{ $horse->photo_path ? asset('storage/' . $horse->photo_path) : asset('storage/horses/contorno.png') }}"
                                 class="w-full h-80 object-cover rounded-lg" />
                         @endif
                     </div>
